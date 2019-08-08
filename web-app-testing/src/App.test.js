b3a84cc7fb565d121fe9b83065ec1b52961e45a7
tania <tanislavens@yahoo.com>
2019-08-08 18:44:38 -0400

    counts work but need more testing

--- web-app-testing/src/App.test.js
+++ web-app-testing/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('renders without crashing using ReactDom', () => {
+    render(<App />);
+  });
+  it('renders Testing', () => {
+    const app = render(<App />);
+    app.getByText(/testing/i);
+  });
 });
